bc86ec318f5cc22c63e04861c01e58f33ee5e0d5
Dean <dmwspace@gmail.com>
2024-05-25 10:03:40 -0700

    start of body and navbar

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+function Body() {
+    return (
+        <div className="body-container">
+            <div className="body-card">
+                <h1>Hello, my name is Dean Wright.</h1>
+                <h1>I am a Frontend Web Developer based in Shoreline, WA.</h1>
+                <h3>I am a thoughtful, creative, and technically savvy web developer specializing in React development.</h3>
+            </div>
+
+        </div>
+    )
+}
+export default Body
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Outlet, Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div id="navbar">
+            <ul>
+                <li><Link to="/" style={{textDecoration: "none", color: "rgb(85,33,137)"}}>Home</Link></li>
+                {/* <li><Link to="/projects" style={{textDecoration: "none", color: "rgb(85,33,137)"}}>Projects</Link></li>
+                <li><Link to="/resume" style={{textDecoration: "none", color: "rgb(85,33,137)"}}>Resume</Link></li>
+                <li><Link to="/skills" style={{textDecoration: "none", color: "rgb(85,33,137)"}}>Skills</Link></li> */}
+            </ul>
+            <br />
+            <Outlet />
+        </div>
+    )
+}
+export default Navbar
